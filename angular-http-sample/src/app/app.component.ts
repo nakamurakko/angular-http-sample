08e5ea08c568fa15e1d13ebd212fececb28ed48c
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2024-12-19 18:03:37 +0900

    Update packages.

--- angular-http-sample/src/app/app.component.ts
+++ angular-http-sample/src/app/app.component.ts
@@ -4,16 +4,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    MatButtonModule,
-    MatToolbarModule,
-    RouterLink,
-    RouterOutlet
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [
+        MatButtonModule,
+        MatToolbarModule,
+        RouterLink,
+        RouterOutlet
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   public title: string = 'angular-http-sample';
